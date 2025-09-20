88daffcd9da306427e5c30ada4a1bd3c4e872249
Miranda Harris <mjharris8515@gmail.com>
2025-09-20 13:36:27 -0500

    Added firebase configuration

--- src/01_firebase/config_firebase.js
+++ src/01_firebase/config_firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlKHCdVfQvMRKQf2B5UqaLfDkwm6T1apo",
-  authDomain: "challo-ghume-fa22c.firebaseapp.com",
-  projectId: "challo-ghume-fa22c",
-  storageBucket: "challo-ghume-fa22c.appspot.com",
-  messagingSenderId: "411994731743",
-  appId: "1:411994731743:web:a02cec85eb6aa66ddbe56a",
-  measurementId: "G-5KYMV9KZ2D"
+  apiKey: "AIzaSyAem4qrql00bMKfo52K4JzX5M8YXcvaalI",
+  authDomain: "expedia-clone-c658b.firebaseapp.com",
+  projectId: "expedia-clone-c658b",
+  storageBucket: "expedia-clone-c658b.firebasestorage.app",
+  messagingSenderId: "28470156977",
+  appId: "1:28470156977:web:83c417c8de563643840c90",
+  measurementId: "G-V46H2WLK8J"
 };
 
 // Initialize Firebase
 const firebase_app = initializeApp(firebaseConfig);
 
-export default firebase_app
+export default firebase_app;
